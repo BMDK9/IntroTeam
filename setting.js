2b63361a8ab566c79722c9d75697466968950e6a
minahYu <mina010311@naver.com>
2023-10-05 20:57:50 +0900

    for pull

---
+++ setting.js
@@ -0,0 +1,20 @@
+        // Firebase SDK 라이브러리 가져오기
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+        import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+        import { doc, collection, addDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+        import { getDocs, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+        // Firebase 구성 정보 설정
+        const firebaseConfig = {
+            apiKey: "AIzaSyAzXX5j6y95F5-w-aemZc--QymtiWT_UVM",
+            authDomain: "intro-team-42d34.firebaseapp.com",
+            projectId: "intro-team-42d34",
+            storageBucket: "intro-team-42d34.appspot.com",
+            messagingSenderId: "367458824490",
+            appId: "1:367458824490:web:b05f0947f7149006206e53",
+            measurementId: "G-0488K83Q4K"
+        };
+
+        // Firebase 인스턴스 초기화
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
